54a74b2763c24b82e01ec2cfb407b04a6f8fb33a
Nick Rigano <nrigano@Executor.local>
2016-10-21 16:49:58 -0400

    progress

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -7,6 +7,19 @@
 //
 
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(password: [Int]) -> Bool
+}
+
+extension getAccess: ProvideAccess {
+    
+    
+//    TRUE if every other digit is even(array positions 1, 3, 5, 7, 9) and array.count == 10
+//    If the person calling on this function doesn't enter in any digits (empty array), then we should return false.
+//    If the total numbers of digits entered (items in the array) exceed 10, then we should return false.
+//    In order to allow access (return true), every other digit (starting with the first digit) must be even. The first digit entered is the first element in the array.
+}
+
 class BankVault {
     
     let name: String
@@ -18,8 +31,11 @@ class BankVault {
         self.address = address
     }
     
+
+    
 }
 
+//extension, should return TRUE: array.position == %0 && array.count <= 10, else return false
 
 
 
